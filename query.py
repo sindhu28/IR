f7f9ae0fe20b779dc53c3453b7ce3c4d7018c3a0
sindhu <vidyasindhu.n@gmail.com>
2012-11-04 01:37:50 -0400

    version 4
    with print(refine)

--- query.py
+++ query.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import urllib2
 import urllib
 
@@ -8,39 +8,45 @@ while query.lower() != "zzz":
     #enter the query
     if ' ' in query:
         print "enter a single term"
-        continue 
-    word = query.lower()
-    #search the query in the index record of the urls
-    f = open('D:/hw/metadata.txt', 'r')
-    pages = []
-    index = 0
-    for line in f.readlines():
-        values = dict(item.split(':',1) for item in line.split('(FIELD)',4))
-        text = values['ANCHOR TEXT']
-        if word in text:
-            url = values['URL']
-            pagerank = values['PAGE RANK']
-            title = values['TITLE']
-            pages.append([url, title, float(pagerank)])
-    #sort the index records in order of page ranks
-    pages.sort(key=lambda x:x[2], reverse=True)
-    print len(pages)
-    f.close()
-    #Print snippet from the urls
-    for page in pages:
-        print page
-        #print "\nURL:", page[0]
-        #print "TITLE:", page[1]
-        #print "PAGERANK", page[2]
-        url = page[0]
-        html = urllib2.urlopen(url)
-        soup = BeautifulSoup(html)
-        text_parts = soup.findAll(text=True)
-        text = ''.join(text_parts)
-        text = text.replace('\n', ' ')
-        text = ' '.join(text.split())
-        text = text.split()
-        text = text[800:810]
-        text = ' '.join(text)
-        #print "CONTENT", text
-    query = raw_input('Enter term : ')
+    else:
+        word = query.lower()
+        #search the query in the index record of the urls
+        f = open('D:/hw/metadata.txt', 'r')
+        pages = []
+        index = 0
+        for line in f.readlines():
+            values = dict(item.split(':',1) for item in line.split('(FIELD)',4))
+            text = values['ANCHOR TEXT']
+            if word in text:
+                url = values['URL']
+                pagerank = values['PAGE RANK']
+                title = values['TITLE']
+                ID = values['ID']
+                pages.append([url, title, float(pagerank), ID])
+        #sort the index records in order of page ranks
+        pages.sort(key=lambda x:x[2], reverse=True)
+        print len(pages)
+        f.close()
+        #Print snippet from the urls
+        index = 0
+        for page in pages:
+            #print "\nURL:", page[0]
+            #print "TITLE:", page[1]
+            #print "PAGERANK", page[2]
+            url = page[0]
+            html = urllib2.urlopen(url)
+            soup = BeautifulSoup(html)
+            ptext = soup.findAll('div')
+            string=''
+            for line in ptext:
+                text = line.findAll(text=True, )
+                text = ''.join(text)
+                text = text.replace('\n', ' ')
+                text = ' '.join(text.split())+' '
+                string = string+text+' '
+            string = string.split()
+            string = string[200:220]
+            string = ' '.join(string)           
+            print '\n', page[3], ":", page[0], "----", page[1]
+            print "CONTENT:", string
+    query = raw_input('\nEnter term : ')
